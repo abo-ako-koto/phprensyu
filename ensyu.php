5bb41bc9d893cbff9a0c5371a06868725fcc89c6
abo-ako-koto <86095814+abo-ako-koto@users.noreply.github.com>
2021-07-08 14:55:38 +0900

    php追加

---
+++ ensyu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせフォーム|架空の株式会社ダミー</title>
+</head>
+<body>
+    <h1>お問い合わせフォーム</h1>
+    <p>株式会社ダミーに関するお問い合わせは<a href="">プライバシーポリシー</a>をご一読の上、下記の入力フォームから送信してください。</p>
+    <table border="3">
+    <form method="POST" action="">
+    <tr><td><p>お名前</p></td>
+    <td><input type="text" name='name' value=""></td></tr>
+    <tr><td><p>メールアドレス</p></td>
+    <td><input type="text" name="mail" value=""></td></tr>
+    <tr><td><p>お問い合わせ内容</p></td>
+    <td><textarea id='t_message' name="message"></textarea></td></tr>
+    </form>
+    </table>
+    <input type="submit" name='botan' value="「入力確認画面へ」">
+    
+
+    <p>© 2016 株式会社ダミー All Rights Reserved.</p>
+</body>
+</html>
